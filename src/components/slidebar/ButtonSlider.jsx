ac96e961d48c6d48da64a21f161def1230a6df33
Alizadehf-laya <alizadehlayaf@gmail.com>
2025-03-12 10:08:39 +0330

    working

--- src/components/slidebar/ButtonSlider.jsx
+++ src/components/slidebar/ButtonSlider.jsx
@@ -1,15 +1,27 @@
 import { ButtonGroup } from 'react-bootstrap';
 import ListGroup from 'react-bootstrap/ListGroup';
+import { House } from 'lucide-react';
+import { User } from 'lucide-react';
+import { Mail } from 'lucide-react';
+import { FileUser } from 'lucide-react';
+import { Newspaper } from 'lucide-react';
+import { EllipsisVertical } from 'lucide-react';
 
 function ButtonSlider() {
   return (
-    <ListGroup className='mt-2' style={{backgroundColor:"rgb(29,28,28)", border:"no-border"}}>
-      <ListGroup.Item style={{backgroundColor:"rgb(29,28,28)", color:"white"}}>Home</ListGroup.Item>
-      <ListGroup.Item  style={{backgroundColor:"rgb(29,28,28)", color:"white"}}>About Us</ListGroup.Item>
-      <ListGroup.Item  style={{backgroundColor:"rgb(29,28,28)", color:"white"}}>Contact Me</ListGroup.Item>
-      <ListGroup.Item  style={{backgroundColor:"rgb(29,28,28)", color:"white"}}>Profile</ListGroup.Item>
-      <ListGroup.Item  style={{backgroundColor:"rgb(29,28,28)", color:"white"}}>Resume</ListGroup.Item>
-      <ListGroup.Item  style={{backgroundColor:"rgb(29,28,28)", color:"white"}}>More</ListGroup.Item>
+    <ListGroup className='border-0' style={{backgroundColor:"rgb(29,28,28)"}}>
+      <ListGroup.Item className='border-0' style={{backgroundColor:"rgb(29,28,28)", color:"white"}}>
+        <House/><span  className='ms-3'>Home</span></ListGroup.Item>
+      <ListGroup.Item className='border-0' style={{backgroundColor:"rgb(29,28,28)", color:"white"}}>
+        <User/><span  className='ms-3'>About</span></ListGroup.Item>
+      <ListGroup.Item className='border-0' style={{backgroundColor:"rgb(29,28,28)", color:"white"}}>
+        <Mail/><span  className='ms-3'>Contact</span></ListGroup.Item>
+      <ListGroup.Item className='border-0' style={{backgroundColor:"rgb(29,28,28)", color:"white"}}>
+        <FileUser/><span  className='ms-3'>Portfolio</span></ListGroup.Item>
+      <ListGroup.Item className='border-0' style={{backgroundColor:"rgb(29,28,28)", color:"white"}}>
+        <Newspaper/><span  className='ms-3'>Resume</span></ListGroup.Item>
+      <ListGroup.Item className='border-0' style={{backgroundColor:"rgb(29,28,28)", color:"white"}}>
+        <EllipsisVertical/><span  className='ms-3'>More</span></ListGroup.Item>
     </ListGroup>
   );
 }
